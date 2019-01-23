c8438ac19fd715cc4870f2193fee55ada49f0fec
sebastiankmiec <sebastian.kmiec@mail.utoronto.ca>
2019-01-22 23:37:03 -0500

    Added a demonstration of the Python package.

--- gui_demo/backgroundworker.py
+++ gui_demo/backgroundworker.py
@@ -96,10 +96,29 @@ class MyoDataWorker(QRunnable):
             for i in range(len(self.series_list)):
                 self.series_list[i].append(self.samples_count, emg_list[i])
 
+            # Accelerometer values are multipled by the following constant (and are in units of g)
+            MYOHW_ACCELEROMETER_SCALE = 2048.0
+
+            # Gyroscope values are multipled by the following constant (and are in units of deg/s)
+            MYOHW_GYROSCOPE_SCALE = 16.0
+
+            # Orientation values are multipled by the following constant (units of a unit quaternion)
+            MYOHW_ORIENTATION_SCALE = 16384.0
+
+            # Update list of all data collected (with correct rescaling)
+            self.data_list.append((time.time(), self.samples_count, emg_list,
+                                        [orient_w / MYOHW_ORIENTATION_SCALE, orient_x / MYOHW_ORIENTATION_SCALE,
+                                         orient_y / MYOHW_ORIENTATION_SCALE, orient_z / MYOHW_ORIENTATION_SCALE],
+                                        [accel_1 / MYOHW_ACCELEROMETER_SCALE, accel_2 / MYOHW_ACCELEROMETER_SCALE,
+                                         accel_3 / MYOHW_ACCELEROMETER_SCALE],
+                                        [gyro_1 / MYOHW_GYROSCOPE_SCALE, gyro_2 / MYOHW_GYROSCOPE_SCALE,
+                                         gyro_3 / MYOHW_GYROSCOPE_SCALE]))
+
             # Update list of all data collected
-            self.data_list.append((time.time(), self.samples_count, emg_list, [orient_w, orient_x, orient_y, orient_z],
-                                                [accel_1, accel_2, accel_3],
-                                                [gyro_1, gyro_2, gyro_3]))
+            # self.data_list.append((time.time(), self.samples_count, emg_list, [orient_w, orient_x, orient_y, orient_z],
+            #                       [accel_1, accel_2, accel_3],
+            #                       [gyro_1, gyro_2, gyro_3]))
+
             self.samples_count += 1
 
             # Request an axes update for the charts
---
+++ main.py
@@ -0,0 +1,42 @@
+from pymyolinux.core.myo import MyoDongle
+
+def joint_event_handler(emg_list, orient_w, orient_x, orient_y, orient_z,
+                                accel_1, accel_2, accel_3, gyro_1, gyro_2, gyro_3):
+
+    # Accelerometer values are multipled by the following constant (and are in units of g)
+    MYOHW_ACCELEROMETER_SCALE = 2048.0
+
+    # Gyroscope values are multipled by the following constant (and are in units of deg/s)
+    MYOHW_GYROSCOPE_SCALE = 16.0
+
+    # Orientation values are multipled by the following constant (units of a unit quaternion)
+    MYOHW_ORIENTATION_SCALE = 16384.0
+
+    print("-------------------------------------------------------------------------------------------")
+    print((emg_list[0], emg_list[1], emg_list[2], emg_list[3], emg_list[4], emg_list[5], emg_list[6], emg_list[7]))
+    #print((orient_w, orient_x, orient_y, orient_z, accel_1, accel_2, accel_3, gyro_1, gyro_2, gyro_3))
+    print((orient_w / MYOHW_ORIENTATION_SCALE, orient_x / MYOHW_ORIENTATION_SCALE, orient_y / MYOHW_ORIENTATION_SCALE,
+                orient_z / MYOHW_ORIENTATION_SCALE, accel_1 / MYOHW_ACCELEROMETER_SCALE,
+                accel_2 / MYOHW_ACCELEROMETER_SCALE, accel_3 / MYOHW_ACCELEROMETER_SCALE,
+                gyro_1 / MYOHW_GYROSCOPE_SCALE, gyro_2 / MYOHW_GYROSCOPE_SCALE, gyro_3 / MYOHW_GYROSCOPE_SCALE))
+
+
+if __name__ == "__main__":
+    device_1 = MyoDongle("/dev/ttyACM0")
+    device_1.clear_state()
+    print("\n")
+
+    myo_devices = device_1.discover_myo_devices()
+    if len(myo_devices) > 0:
+        device_1.connect(myo_devices[0])
+    else:
+        print("No devices found, exiting...")
+        exit()
+
+    #device_1.add_imu_handler()
+    #device_1.add_emg_handler()
+    device_1.enable_imu_readings()
+    device_1.enable_emg_readings()
+    device_1.add_joint_emg_imu_handler(joint_event_handler)
+
+    device_1.scan_for_data_packets()
